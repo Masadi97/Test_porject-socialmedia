7fed9ba2e20602e4babe6d5e1e9e49157b29d4b3
Masadi97 <132331927+Masadi97@users.noreply.github.com>
2024-06-01 15:45:01 +0330

    Add files via upload

---
+++ home/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Post, Comment
+
+@admin.register(Post)  ### admin.site.register(Post, PostAdmin) ###
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('user', 'slug', 'updated')
+    search_fields = ('slug', 'body')
+    list_filter = ('updated',)
+    prepopulated_fields = {'slug': ('body',)}
+    raw_id_fields = ('user',)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'post', 'created', 'is_reply')
+    raw_id_fields = ('user', 'post', 'reply')
---
+++ home/views.py
@@ -0,0 +1,84 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import View
+from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+from .forms import PostCreateUpdateForm, CommentCreateForm
+from django.utils.text import slugify
+
+
+class HomeView(View):
+    def get(self, request):
+        posts = Post.objects.all()
+        return render(request, 'home/index.html', {'posts': posts})
+
+
+class PostDetailView(View):
+    form_class = CommentCreateForm
+    def get(self, request, post_id, post_slug):
+        post = get_object_or_404(Post, pk=post_id, slug=post_slug)
+        comment = post.pcomments.filter(is_reply=False)
+
+        return render(request, 'home/detail.html', {'post': post, 'comment': comment, 'form': self.form_class})
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            
+
+
+class PostDeleteView(LoginRequiredMixin, View):
+    def get(self, request, post_id):
+        post = get_object_or_404(Post, pk=post_id)
+        if post.user.id == request.user.id:
+            post.delete()
+            messages.success(request, 'post deleted successfuly', 'success')
+        else:
+            messages.error(request, 'you can not delete this post', 'danger')
+        return redirect('home:home')
+
+
+class PostUpdateView(LoginRequiredMixin, View):
+    form_class = PostCreateUpdateForm
+
+    def setup(self, request, *args, **kwargs):
+        self.post_instance = get_object_or_404(Post, pk=kwargs['post_id'])
+        return super().setup(request, *args, **kwargs)
+
+
+    def dispatch(self, request, *args, **kwargs):
+        post = self.post_instance
+        if not post.user.id == request.user.id:
+            messages.error(request, 'you can not update this post', 'danger')
+            return redirect('home:home')
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        post = self.post_instance
+        form = self.form_class(instance=post)
+        return render(request, 'home/update.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        post = self.post_instance
+        form = self.form_class(request.POST, instance=post)
+        if form.is_valid():
+            new_post = form.save(commit=False)
+            new_post.slug = slugify(form.cleaned_data['body'][:30])
+            new_post.save()
+            messages.success(request, 'you update this post', 'success')
+            return redirect('home:post_detail', post_id, post.slug)
+
+class PostCreateView(LoginRequiredMixin, View):
+    form_class = PostCreateUpdateForm
+    def get(self, request, *args, **kwargs):
+        form = self.form_class
+        return render(request, 'home/create.html', {'form':form})
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            new_post = form.save(commit=False)
+            new_post.slug = slugify(form.cleaned_data['body'][:30])
+            new_post.user = request.user
+            new_post.save()
+            messages.success(request, 'you create post successfully', 'success')
+            return redirect('home:post_detail', new_post.id, new_post.slug)
